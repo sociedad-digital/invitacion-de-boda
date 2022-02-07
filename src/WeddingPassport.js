4b3f4b7598cb617716e33e0836b60ab48316845e
Emir Herrera <emir.herrera@gmail.com>
2022-02-07 17:36:40 -0600

    add: dev: Data source segregated

--- src/WeddingPassport.js
+++ src/WeddingPassport.js
@@ -1,10 +1,14 @@
 import './WeddingPassport.css';
+import novia from './data/novia.json' ;
+import novio from './data/novio.json' ;
 
 function WeddingPassport() {
+
   return (
     <div className="WeddingPassport">
       <div className="WeddingPassport-cover">
-        <h1>PASAPORTE</h1>
+        <h1>{novia.nombre} & {novio.nombre}</h1>
+        <p>PASAPORTE</p>
       </div>
     </div>
   );
