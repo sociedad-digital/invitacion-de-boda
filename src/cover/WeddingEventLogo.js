c41fd84e6dffce769182dee2e06d4fa56aa1b6cd
Emir Herrera <emir.herrera@gmail.com>
2022-02-09 15:39:41 -0600

    chg: dev: Split WeddingEventLogo Stylesheet

--- src/cover/WeddingEventLogo.js
+++ src/cover/WeddingEventLogo.js
@@ -1,10 +1,11 @@
 import WeddingBanner from './WeddingBanner';
+import './WeddingEventLogo.css'
 
 function WeddingEventLogo() {
   return (
     <div className="WeddingEventLogo">
       <img className="frame" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-frame-border-png_5604780.png"} />
-      <WeddingBanner />
+      <WeddingBanner/>
     </div>
   );
 }
