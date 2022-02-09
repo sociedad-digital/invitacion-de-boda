51b4e960d68a5359874cdd73dcea601c1aeb0576
Emir Herrera <emir.herrera@gmail.com>
2022-02-09 15:11:54 -0600

    fix: dev: !minor class usage is discoraged in favor of className
    JSX usess className not class wich is an HTML attribute
    
    https://stackoverflow.com/questions/30968113/warning-unknown-dom-property-class-did-you-mean-classname

--- src/cover/WeddingEventLogo.js
+++ src/cover/WeddingEventLogo.js
@@ -1,11 +1,10 @@
 function WeddingEventLogo() {
   return (
-    <div class="WeddingEventLogo">
-      <img class="frame" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-frame-border-png_5604780.png"} />
-      <img class="banner" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-ribbon-png_93008.png"} />
+    <div className="WeddingEventLogo">
+      <img className="frame" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-frame-border-png_5604780.png"} />
+      <img className="banner" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-ribbon-png_93008.png"} />
     </div>
   );
 }
-
 
 export default WeddingEventLogo
