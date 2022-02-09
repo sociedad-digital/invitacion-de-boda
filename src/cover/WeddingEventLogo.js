f73b3a5a5a57ecf44ea58617f51b83810bebc06d
Emir Herrera <emir.herrera@gmail.com>
2022-02-09 04:41:09 -0600

    chg: user: !style Alineación de elementos

---
+++ src/cover/WeddingEventLogo.js
@@ -0,0 +1,11 @@
+function WeddingEventLogo() {
+  return (
+    <div class="WeddingEventLogo">
+      <img class="frame" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-frame-border-png_5604780.png"} />
+      <img class="banner" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-ribbon-png_93008.png"} />
+    </div>
+  );
+}
+
+
+export default WeddingEventLogo
--- src/cover/WeddingPassportCover.js
+++ src/cover/WeddingPassportCover.js
@@ -1,11 +1,13 @@
 import novia from '../data/novia.json' ;
 import novio from '../data/novio.json' ;
-import './WeddingPassportCover.css'
+import './WeddingPassportCover.css';
+import WeddingEventLogo from './WeddingEventLogo';
 
 function WeddingPassportCover() {
   return (
     <div className="WeddingPassport-cover">
-      <h1>{novia.nombre} & {novio.nombre}</h1>
+      <h1>{novia.nombre} <br/>&<br/> {novio.nombre}</h1>
+      <WeddingEventLogo />
       <p>PASAPORTE</p>
     </div>
   )
