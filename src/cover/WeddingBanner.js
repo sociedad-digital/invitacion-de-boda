f1dc95bfd0d465fcd27195d856d88cb65a47c1f8
Emir Herrera <emir.herrera@gmail.com>
2022-02-09 15:29:40 -0600

    chg: dev: Modularize WeddingBanner

---
+++ src/cover/WeddingBanner.js
@@ -0,0 +1,7 @@
+function WeddingBanner(){
+  return (
+    <img className="banner" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-ribbon-png_93008.png"} />
+  );
+}
+
+export default WeddingBanner;
--- src/cover/WeddingEventLogo.js
+++ src/cover/WeddingEventLogo.js
@@ -1,8 +1,10 @@
+import WeddingBanner from './WeddingBanner';
+
 function WeddingEventLogo() {
   return (
     <div className="WeddingEventLogo">
       <img className="frame" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-frame-border-png_5604780.png"} />
-      <img className="banner" src={process.env.PUBLIC_URL + "/assets/NicePng_gold-ribbon-png_93008.png"} />
+      <WeddingBanner />
     </div>
   );
 }
