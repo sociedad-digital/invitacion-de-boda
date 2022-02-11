63f7259ab8457b75530d602065d2b50a022b4682
Emir Herrera <emir.herrera@gmail.com>
2022-02-11 00:49:52 -0600

    chg: dev: Use data source for couple name

--- src/cover/WeddingFrame.js
+++ src/cover/WeddingFrame.js
@@ -1,7 +1,10 @@
 import './WeddingFrame.css';
+import novia from '../data/novia.json';
+import novio from '../data/novio.json';
+
 
 function WeddingFrame(){
-  var couple = "N & N"
+  var couple = `${novio.nombre[0]} & ${novia.nombre[0]}`
   return (
     <div className="frame">
       <h1>{couple}</h1>
--- src/cover/WeddingPassportCover.js
+++ src/cover/WeddingPassportCover.js
@@ -1,12 +1,16 @@
 import './WeddingPassportCover.css';
 import WeddingEventLogo from './WeddingEventLogo';
+import novio from '../data/novio.json';
+import novia from '../data/novia.json';
 
 function WeddingPassportCover() {
+
   return (
     <div className="WeddingPassport-cover">
-    <h1>Huateque Nupcial</h1>
+    <h1 className="passport">PASAP♥RTE</h1>
       <WeddingEventLogo />
-      <p>PASAPORTE</p>
+    <h6>A nuestra boda</h6>
+    <h1 className="couple">{novio.nombre} & {novia.nombre}</h1>
     </div>
   )
 }
