86ed5cb30f5a38f90124c12f33c11e7fae3c8d1e
Emir Herrera <emir.herrera@gmail.com>
2022-02-11 22:39:07 -0600

    new: dev: Flip passport cover animation

--- src/WeddingPassport.js
+++ src/WeddingPassport.js
@@ -1,11 +1,26 @@
 import './WeddingPassport.css';
-import WeddingPassportCover from './cover/WeddingPassportCover'
-function WeddingPassport() {
+import WeddingPassportCover from './cover/WeddingPassportCover';
+import HTMLFlipBook from "react-pageflip";
 
+function WeddingPassport(this: any) {
   return (
-    <div className="WeddingPassport">
+    <HTMLFlipBook
+      width={88}
+      height={125}
+      size="stretch"
+      minWidth={315}
+      maxWidth={1000}
+      minHeight={400}
+      maxHeight={1533}
+      maxShadowOpacity={0.5}
+      showCover={true}
+      mobileScrollSupport={true}
+      className="WeddingPassport"
+    >
       <WeddingPassportCover />
-    </div>
+      <div className="demoPage">Back</div>
+    </HTMLFlipBook>
+
   );
 }
 
--- src/cover/WeddingPassportCover.js
+++ src/cover/WeddingPassportCover.js
@@ -2,17 +2,18 @@ import './WeddingPassportCover.css';
 import WeddingEventLogo from './WeddingEventLogo';
 import novio from '../data/novio.json';
 import novia from '../data/novia.json';
+import React from 'react';
 
-function WeddingPassportCover() {
+const WeddingPassportCover = React.forwardRef((props, ref) => {
 
   return (
-    <div className="WeddingPassport-cover">
+    <div className="WeddingPassport-cover" ref={ref} >
     <h1 className="passport">PASAP♥RTE</h1>
       <WeddingEventLogo />
     <h6>A nuestra boda</h6>
     <h1 className="couple">{novio.nombre} & {novia.nombre}</h1>
     </div>
   )
-}
+});
 
 export default WeddingPassportCover;
