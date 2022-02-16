40fb232628fc980e4531cf4b32192c2c307d656f
Emir Herrera <emir.herrera@gmail.com>
2022-02-16 04:28:34 -0600

    new: user: Page horientation

--- src/reception/WeddingReception.js
+++ src/reception/WeddingReception.js
@@ -5,33 +5,35 @@ import './WeddingReception.css';
 const WeddingReception = React.forwardRef((props, ref) => {
   return (
     <div ref={ref} className={props.className}>
-      <h2>Pasaporte de boda</h2>
-      <img className="featuring-photo" src={boda.featuring_photo} />
-      <div className="event-details">
-        <div className="row">
-          <span className="Type">Recepción</span>
-          <span className="Code">{boda.recepcion_lugar}</span>
-          <span className="PassportID">{boda.recepcion_pase}</span>
+      <div className="horizontal-content">
+        <h2>Pasaporte de boda</h2>
+        <img className="featuring-photo" src={boda.featuring_photo} />
+        <div className="event-details">
+          <div className="row">
+            <span className="Type">Recepción</span>
+            <span className="Code">{boda.recepcion_lugar}</span>
+            <span className="PassportID">{boda.recepcion_pase}</span>
+          </div>
+          <div className="row">
+            <span className="Name">{boda.novia.nombre} & {boda.novio.nombre}</span>
+          </div>
+          <div className="row">
+            <span className="Date">{boda.fecha_recepción}</span>
+          </div>
+          <div className="row">
+            <span className="Time">{boda.recepcion_hora}</span>
+          </div>
+          <div className="row">
+            <span className="Location">{boda.recepcion_lugar}</span>
+          </div>
         </div>
-        <div className="row">
-          <span className="Name">{boda.novia.nombre} & {boda.novio.nombre}</span>
-        </div>
-        <div className="row">
-          <span className="Date">{boda.fecha_recepción}</span>
-        </div>
-        <div className="row">
-          <span className="Time">{boda.recepcion_hora}</span>
-        </div>
-        <div className="row">
-          <span className="Location">{boda.recepcion_lugar}</span>
-        </div>
-      </div>
 
-      <img src="heart-stamp.jpg" />
+        <img src="heart-stamp.jpg" />
 
-      <div className="arrivalCode">
-        <p>¡Buenaventura a la feliz pareja!</p>
-        <p>¡Apreciamos vuestra asistencia!</p>
+        <div className="arrivalCode">
+          <p>¡Buenaventura a la feliz pareja!</p>
+          <p>¡Apreciamos vuestra asistencia!</p>
+        </div>
       </div>
     </div>
   );
