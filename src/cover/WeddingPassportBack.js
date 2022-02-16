6a80cb22499fe047578e468e4e7d35053c6b733f
Emir Herrera <emir.herrera@gmail.com>
2022-02-16 04:38:20 -0600

    new: user: !release Simple paper style

--- src/cover/WeddingPassportBack.js
+++ src/cover/WeddingPassportBack.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './WeddingPassportBack.css';
 
 const WeddingPassportBack = React.forwardRef((props, ref) => {
   return (
-    <div  ref={ref}>
+    <div  className="WeddingPassport-back" ref={ref}>
       <h3>¡No olvides reservar tu platillo!</h3>
       <h4>Mesa de regalos, ¡En la cocina!</h4>
       <h4>¡Regalanos una receta!</h4>
