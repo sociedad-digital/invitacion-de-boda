53525d6f030844eafc6eaa6f8949ffc6f746499e
Emir Herrera <emir.herrera@gmail.com>
2022-02-20 17:02:26 -0600

    chg: user: Back logo set to use the WeddingEventLogo

--- src/cover/WeddingPassportBack.js
+++ src/cover/WeddingPassportBack.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './WeddingPassportBack.css';
-import CoupleInitials from './CoupleInitials';
-import WeddingDate from './WeddingDate';
 import './WeddingEventLogo.css';
+import WeddingEventLogo from './WeddingEventLogo';
+
 const WeddingPassportBack = React.forwardRef((props, ref) => {
   return (
     <div  className={props.className} ref={ref}>
       <div className="centered-container">
-        <div className="back-logo">
-          <CoupleInitials className="fancy-font logo-component"/>
-          <WeddingDate className="fancy-font logo-component" />
-        </div>
+        <WeddingEventLogo />
         <h6>¡Mesa de regalos en la cocina!</h6>
         <h6>¡Regálanos una receta!</h6>
       </div>
